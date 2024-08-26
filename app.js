33ed10857b44586b9d391699c9cfa06c4e7152ce
Piyush <piyushpatel9165@gmail.com>
2024-08-26 22:31:32 +0530

    pull

--- app.js
+++ app.js
@@ -21,18 +21,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+try {
+  let Mongourl = "mongodb://127.0.0.1:27017/Renify";
+
+  mongoose.connect(Mongourl)
+  console.log("DB connection success")
+} catch (err) {
+  console.log("dataBase error" + err)
+}
 
 app.listen(3000)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
