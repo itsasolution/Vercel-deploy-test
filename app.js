9c6bb506305e585e8830cea1b187ea0bbee5bb83
Piyush <piyushpatel9165@gmail.com>
2024-08-26 21:57:11 +0530

    vercel deploy test

--- app.js
+++ app.js
@@ -21,10 +21,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+app.listen(3000)
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -33,8 +38,9 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 400);
   res.render('error');
 });
+
 
 module.exports = app;
