b1af7770cae0955f9b2fc92b67ca7f3502f08d84
Piyush <piyushpatel9165@gmail.com>
2024-08-28 19:17:55 +0530

    inde

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,45 @@
+
+var userrouter = require("./users")
 var express = require('express');
 var router = express.Router();
-var nodemailer = require("./nodemailer")
-var userrouter = require("./users")
+var vehilclesRouter = require('./vehilclesRouter');
+var providerRouter = require('./providerRouter');
+var userRouter = require('./userRouter');
+var addVehicles = require('./ADDVEHICLES');
 
-router.use("/mail", nodemailer)
+const { default: mongoose } = require('mongoose');
 
-/* GET home page. */
-router.use("/users", userrouter)
-router.get('/test', function (req, res, next) {
+try {
+  let Mongourl = process.env.mongoUrl || "mongodb://127.0.0.1:27017/Renify";
+
+  mongoose.connect(Mongourl)
+  console.log("DB connection success")
+} catch (err) {
+  console.log("dataBase error" + err)
+}
+
+router.use("/user", userrouter)
+
+router.get('/', function (req, res, next) {
   res.send(`<h2>Testing Okk</h2>`)
 });
 router.get('/index', function (req, res, next) {
   res.send(`<h2>Index</h2>`)
 });
+
+router.get("/", (req, res) => {
+  res.status(200).send("Everthing is working fine...")
+})
+
+router.get("/cookie", (req, res) => {
+  res.cookie("name", "Piyush")
+  res.send("cookie tutorial Press 'i' on left of localhost:4000\nused to end small piece of data to server ")
+})
+
+module.exports = router;
+
+
+
 
 
 
