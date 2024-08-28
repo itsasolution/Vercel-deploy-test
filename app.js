652ab3f41424db1584dcc6bec75404195925e73b
Piyush <piyushpatel9165@gmail.com>
2024-08-28 19:11:28 +0530

    inde

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const cors = require("cors")
 var app = express();
 
-// view engine setup 
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
+// const FRONTEND_ORIGIN = 'http://localhost:3000';
+// const FRONTEND_ORIGIN = 'https://renify.vercel.app';
+
+// app.use(cors({
+//   origin: FRONTEND_ORIGIN,
+//   credentials: true // This ensures cookies are included in the requests
+// }));
+
+
+// _____________________________________
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,14 +31,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-app.listen(3000)
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
-
 
 // error handler
 app.use(function (err, req, res, next) {
@@ -36,9 +49,8 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 400);
+  res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;
